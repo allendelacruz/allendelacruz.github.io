fde6fcc1a02a0ed310abd1a1fa526ad42f49fbf2
allendelacruz <42735353+allendelacruz@users.noreply.github.com>
2021-05-26 16:47:37 +0800

    Create ProjectThumb component.

---
+++ allendelacruz.github.io/Models/ProjectThumbDto.cs
@@ -0,0 +1,9 @@
+﻿namespace allendelacruz.github.io.Models
+{
+    public class ProjectThumbDto
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
