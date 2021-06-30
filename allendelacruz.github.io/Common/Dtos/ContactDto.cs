4a946d10fa88344e8f0e65a8dc5ed5a7903852ac
allendelacruz <42735353+allendelacruz@users.noreply.github.com>
2021-06-30 22:48:44 +0800

    Update index and add contact page.

---
+++ allendelacruz.github.io/Common/Dtos/ContactDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace allendelacruz.github.io.Common.Dtos
+{
+    public class ContactDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
