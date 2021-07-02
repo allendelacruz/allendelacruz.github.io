42b8df1d4e37c8e052bd3734aab02df7240155d2
allendelacruz <42735353+allendelacruz@users.noreply.github.com>
2021-07-02 13:47:46 +0800

    add sending capability at contact page

--- allendelacruz.github.io/Common/Dtos/ContactDto.cs
+++ allendelacruz.github.io/Common/Dtos/ContactDto.cs
@@ -1,19 +1,24 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace allendelacruz.github.io.Common.Dtos
 {
     public class ContactDto
     {
         [Required]
+        [JsonPropertyName("name")]
         public string Name { get; set; }
 
         [Required]
+        [JsonPropertyName("email")]
         public string Email { get; set; }
 
         [Required]
+        [JsonPropertyName("subject")]
         public string Subject { get; set; }
 
         [Required]
+        [JsonPropertyName("message")]
         public string Message { get; set; }
     }
 }
