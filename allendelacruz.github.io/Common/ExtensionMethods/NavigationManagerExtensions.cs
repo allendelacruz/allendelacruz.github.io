455348929e2b3c23389169492de1d663293b98a1
allendelacruz <42735353+allendelacruz@users.noreply.github.com>
2021-07-06 21:20:18 +0800

    add query string at filter by at projects.razor

---
+++ allendelacruz.github.io/Common/ExtensionMethods/NavigationManagerExtensions.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.WebUtilities;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace allendelacruz.github.io.Common.ExtensionMethods
+{
+    public static class NavigationManagerExtensions
+    {
+        public static bool TryGetQueryString<T>(this NavigationManager navManager, string key, out T value)
+        {
+            var uri = navManager.ToAbsoluteUri(navManager.Uri);
+
+            if (QueryHelpers.ParseQuery(uri.Query).TryGetValue(key, out var valueFromQueryString))
+            {
+                if (typeof(T) == typeof(int) && int.TryParse(valueFromQueryString, out var valueAsInt))
+                {
+                    value = (T)(object)valueAsInt;
+                    return true;
+                }
+
+                if (typeof(T) == typeof(string))
+                {
+                    value = (T)(object)valueFromQueryString.ToString();
+                    return true;
+                }
+
+                if (typeof(T) == typeof(decimal) && decimal.TryParse(valueFromQueryString, out var valueAsDecimal))
+                {
+                    value = (T)(object)valueAsDecimal;
+                    return true;
+                }
+            }
+
+            value = default;
+            return false;
+        }
+
+        public static string AddQueryString(this string uri, IDictionary<string, string> queryString)
+            => QueryHelpers.AddQueryString(uri, queryString);
+
+        public static string RemoveQueryStringByKey(this NavigationManager navManager, string key)
+        {
+            var uri = navManager.ToAbsoluteUri(navManager.Uri);
+
+            // this gets all the query string key value pairs as a collection
+            var newQueryString = HttpUtility.ParseQueryString(uri.Query);
+
+            // this removes the key if exists
+            newQueryString.Remove(key);
+
+            // this gets the page path from root without QueryString
+            string pagePathWithoutQueryString = uri.GetLeftPart(UriPartial.Path);
+
+            return newQueryString.Count > 0
+                ? string.Format("{0}?{1}", pagePathWithoutQueryString, newQueryString)
+                : pagePathWithoutQueryString;
+        }
+
+        public static void NavigatewithQueryString(this NavigationManager navManager, string uri, IDictionary<string, string> queryString)
+            => navManager.NavigateTo(uri.AddQueryString(queryString));
+    }
+}
